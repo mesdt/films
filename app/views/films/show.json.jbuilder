3bbd3c45c235c2d340d31d876c8624ce6de17a18
Sasha Go <a@ptx.su>
2015-04-15 00:58:41 +0600

    bin/rails generate scaffold Film year:string title:string; bin/rake db:migrate

---
+++ app/views/films/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @film, :id, :year, :title, :created_at, :updated_at
---
+++ db/migrate/20150414184216_create_films.rb
@@ -0,0 +1,10 @@
+class CreateFilms < ActiveRecord::Migration
+  def change
+    create_table :films do |t|
+      t.string :year
+      t.string :title
+
+      t.timestamps null: false
+    end
+  end
+end
